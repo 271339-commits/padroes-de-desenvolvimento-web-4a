2d7722bae3f61a05bacbcfe38fed53c7b79f1d44
mateusnovo99 <madeiradasikva@gmail.com>
2025-10-29 21:58:27 +0000

    29/10/2025

---
+++ projeto/src/main/java/recursos/ItemVendaRecurso.java
@@ -0,0 +1,15 @@
+package recursos;
+
+import java.util.List;
+
+import entidades.ItemVenda;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+
+@Path("itensvenda")
+public class ItemVendaRecurso {
+    @GET
+    public List<ItemVenda> listar() {
+        return ItemVenda.listAll();
+    }
+}
